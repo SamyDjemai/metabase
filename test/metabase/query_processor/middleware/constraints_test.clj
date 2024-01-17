SamyDjemai/metabase

commit 057149b606668a06da52bbaa16c7d56617387824

make max results configurable (#35600)
This replaces the existing setting max-results-bare-rows with two settings:

- `max-unaggregated-query-row-limit`, and
- `max-aggregated-query-row-limit`

src/metabase/query_processor/middleware/constraints.clj
@@ -5,33 +5,59 @@
    [metabase.models.setting :as setting]
    [metabase.util.i18n :refer [deferred-tru]]))
 
-(def ^:private ^:const default-max-results-bare-rows 2000)
+;; The following "defaults" are not applied to the settings themselves - why not? Because the existing behavior is
+;; that, if you manually update the settings, queries are affected *WHETHER OR NOT* the
+;; `add-default-userland-constraints` middleware was applied.
+;;
+;; To achieve this, the QP looks for the following, in order:
+;; 1. a non-nil value set by the `add-default-userland-constraints` middleware below, either:
+;;    a) the value of the setting (if it's set),
+;;    b) the "default" value from the constant below, or
+;;    c) `nil` if the constraint middleware was not applied
+;; 2. a non-nil value for the appropriate setting (for aggregated vs. unaggregated queries) itself, either:
+;;    a) the value of the setting, or
+;;    b) `nil` if the setting is not set
+;; 3. the value of `absolute-max-results`
+;;
+;; If we turned the below `const`s into `:default`s on the settings themselves, we would use the default values for
+;; all queries, whether or not the middleware was applied.
+(def ^:private ^:const default-max-unaggregated-query-row-limit 2000)
+(def ^:private ^:const default-max-aggregated-query-row-limit 10000)
 
-;; NOTE: this was changed from a hardcoded var with value of 2000 (now moved to [[default-max-results-bare-rows]])
+;; NOTE: this was changed from a hardcoded var with value of 2000 (now moved to [[default-max-unaggregated-query-row-limit]])
 ;; to a setting in 0.43 the setting, which allows for DB local value, can still be nil, so any places below that used
 ;; to reference the former constant value have to expect it could return nil instead
-(setting/defsetting max-results-bare-rows
+(setting/defsetting max-unaggregated-query-row-limit
   (deferred-tru "Maximum number of rows to return specifically on :rows type queries via the API.")
   :visibility     :authenticated
   :type           :integer
   :database-local :allowed
   :audit          :getter)
 
-(def ^:private max-results
-  "General maximum number of rows to return from an API query."
-  10000)
+(setting/defsetting max-aggregated-query-row-limit
+  (deferred-tru "Maximum number of rows to return for aggregated queries via the API.")
+  :visibility     :authenticated
+  :type           :integer
+  :database-local :allowed
+  :audit          :getter)
+
+(defn query->max-rows
+  "Given a query, returns the max rows that should be returned *as defined by settings*. In other words,
+  return `(max-aggregated-query-row-limit)` or `(max-unaggregated-query-row-limit)` depending on whether the query is
+  aggregated or not."
+  [{{aggregations :aggregation} :query}]
+  (if-not aggregations
+    (max-unaggregated-query-row-limit)
+    (max-aggregated-query-row-limit)))
 
 (defn default-query-constraints
   "Default map of constraints that we apply on dataset queries executed by the api."
   []
-  {:max-results           max-results
-   :max-results-bare-rows (or (max-results-bare-rows) default-max-results-bare-rows)})
+  {:max-results           (or (max-aggregated-query-row-limit) default-max-aggregated-query-row-limit)
+   :max-results-bare-rows (or (max-unaggregated-query-row-limit) default-max-unaggregated-query-row-limit)})
 
 (defn- ensure-valid-constraints
-  "`:max-results-bare-rows` must be less than or equal to `:max-results`, so if someone sets `:max-results` but not
-  `:max-results-bare-rows` or sets an both but sets an invalid value for ``:max-results-bare-rows` use the same value
-  for both. Otherwise the default bare rows value could end up being higher than the custom `:max-rows` value, causing
-  an error."
+  "Clamps the value of `max-results-bare-rows` to be less than or equal to the value of `max-results`."
   [{:keys [max-results max-results-bare-rows], :as constraints}]
   (if (<= max-results-bare-rows max-results)
     constraints

src/metabase/query_processor/middleware/limit.clj
@@ -28,13 +28,13 @@
   "Given a `query`, return the max rows that should be returned, or `nil` if no limit should be applied.
   If a limit should be applied, this is the first non-nil value from (in decreasing priority order):
 
-  1. the value of the [[metabase.query-processor.middleware.constraints/max-results-bare-rows]] setting, which allows
+  1. the value of the [[metabase.query-processor.middleware.constraints/query->max-rows]] setting, which allows
      for database-local override
   2. the output of [[metabase.mbql.util/query->max-rows-limit]] when called on the given query
   3. [[metabase.query-processor.interface/absolute-max-results]] (a constant, non-nil backstop value)"
   [query]
   (when-not (disable-max-results? query)
-    (or (qp.constraints/max-results-bare-rows)
+    (or (qp.constraints/query->max-rows query)
         (mbql.u/query->max-rows-limit query)
         qp.i/absolute-max-results)))
 

test/metabase/query_processor/middleware/constraints_test.clj
@@ -15,8 +15,8 @@
 (deftest ^:parallel add-constraints-test
   (testing "if it is *truthy* add the constraints"
     (is (= {:middleware  {:add-default-userland-constraints? true},
-            :constraints {:max-results           @#'qp.constraints/max-results
-                          :max-results-bare-rows @#'qp.constraints/default-max-results-bare-rows}}
+            :constraints {:max-results           @#'qp.constraints/default-max-aggregated-query-row-limit
+                          :max-results-bare-rows @#'qp.constraints/default-max-unaggregated-query-row-limit}}
            (add-default-userland-constraints
             {:middleware {:add-default-userland-constraints? true}})))))
 
@@ -29,7 +29,7 @@
 (deftest ^:parallel dont-overwrite-existing-constraints-test
   (testing "if it already has constraints, don't overwrite those!"
     (is (= {:middleware  {:add-default-userland-constraints? true}
-            :constraints {:max-results           @#'qp.constraints/max-results
+            :constraints {:max-results           @#'qp.constraints/default-max-aggregated-query-row-limit
                           :max-results-bare-rows 1}}
            (add-default-userland-constraints
             {:constraints {:max-results-bare-rows 1}

modules/drivers/sqlserver/src/metabase/driver/sqlserver.clj
@@ -690,5 +690,5 @@
     ;; if the user has set the rowcount-override connection property, it ends up in the details map, but it actually
     ;; needs to be moved over to the settings map (which is where DB local settings go, as per #19399)
     (-> (update database :details #(dissoc % :rowcount-override))
-        (update :settings #(assoc % :max-results-bare-rows rowcount-override)))
+        (update :settings #(assoc % :max-unaggregated-query-row-limit rowcount-override)))
     database))

src/metabase/models/setting.clj
@@ -188,7 +188,8 @@
      humanization-strategy
      landing-page
      loading-message
-     max-results-bare-rows
+     max-aggregated-query-row-limit
+     max-unaggregated-query-row-limit
      native-query-autocomplete-match-style
      persisted-models-enabled
      report-timezone

test/metabase/api/database_test.clj
@@ -1809,33 +1809,33 @@
           (is (nil? (settings))))
         (testing "Set initial value"
           (testing "response"
-            (is (partial= {:settings {:max-results-bare-rows 1337}}
-                          (set-settings! {:max-results-bare-rows 1337}))))
+            (is (partial= {:settings {:max-unaggregated-query-row-limit 1337}}
+                          (set-settings! {:max-unaggregated-query-row-limit 1337}))))
           (testing "App DB"
-            (is (= {:max-results-bare-rows 1337}
+            (is (= {:max-unaggregated-query-row-limit 1337}
                    (settings)))))
         (testing "Setting a different value should not affect anything not specified (PATCH-style update)"
           (testing "response"
-            (is (partial= {:settings {:max-results-bare-rows   1337
+            (is (partial= {:settings {:max-unaggregated-query-row-limit   1337
                                       :database-enable-actions true}}
                           (set-settings! {:database-enable-actions true}))))
           (testing "App DB"
-            (is (= {:max-results-bare-rows   1337
+            (is (= {:max-unaggregated-query-row-limit   1337
                     :database-enable-actions true}
                    (settings)))))
         (testing "Update existing value"
           (testing "response"
-            (is (partial= {:settings {:max-results-bare-rows   1337
+            (is (partial= {:settings {:max-unaggregated-query-row-limit   1337
                                       :database-enable-actions false}}
                           (set-settings! {:database-enable-actions false}))))
           (testing "App DB"
-            (is (= {:max-results-bare-rows   1337
+            (is (= {:max-unaggregated-query-row-limit   1337
                     :database-enable-actions false}
                    (settings)))))
         (testing "Unset a value"
           (testing "response"
             (is (partial= {:settings {:database-enable-actions false}}
-                          (set-settings! {:max-results-bare-rows nil}))))
+                          (set-settings! {:max-unaggregated-query-row-limit nil}))))
           (testing "App DB"
             (is (= {:database-enable-actions false}
                    (settings)))))))))

test/metabase/models/database_test.clj
@@ -75,16 +75,16 @@
                        {:description nil
                         :name        "testpg"
                         :details     {}
-                        :settings    {:database-enable-actions true ; visibility: :public
-                                      :max-results-bare-rows 2000}  ; visibility: :authenticated
+                        :settings    {:database-enable-actions          true   ; visibility: :public
+                                      :max-unaggregated-query-row-limit 2000}  ; visibility: :authenticated
                         :id          3})]
     (testing "authenticated users should see settings with authenticated visibility"
       (mw.session/with-current-user
         (mt/user->id :rasta)
         (is (= {"description" nil
                 "name"        "testpg"
-                "settings"    {"database-enable-actions" true
-                               "max-results-bare-rows"  2000}
+                "settings"    {"database-enable-actions"          true
+                               "max-unaggregated-query-row-limit" 2000}
                 "id"          3}
                (encode-decode pg-db)))))
     (testing "non-authenticated users shouldn't see settings with authenticated visibility"

commit 181a6af660d45a1b8a92f63adc88ec87df2dca27

Fix db-level settings for max results
This is a bit of a rat's nest. If there's a better way to approach the
problem, I'd be happy to hear it.

The main difficulty is that the userland middleware does not have access
to db-local settings. So if we want db-local settings to apply only to
userland, we have to hack our way around it.

The solution here is to divide the userland middleware into two parts:

- the actually-userland bit, which just marks the query as needing
default limits, and

- a middleware inserted deeper in the middleware stack (just before we
actually apply limits) that actually calculates those limits and
attaches them to the query.

While testing this, I discovered that we have a longstanding bug related
to SQLServer.

Our
documentation (https://www.metabase.com/docs/latest/databases/connections/sql-server)
suggests that a rowcount override of 0 will mean that query results are
"unconstrained."

However, setting the rowcount override to 0 actually results in... empty
results. I made sure that this wasn't caused by me in #35600 by checking
out the preceding commit and verifying that the issue was still there -
it was.

If the db-local setting is set to something other than 0, things seem to
work as intended. For example, with a "ROWCOUNT Override" of 10:

- when viewing a question in the UI, only 10 rows are returned

- when downloading a CSV of results, unlimited results are returned

src/metabase/query_processor.clj
@@ -149,6 +149,7 @@
    #'auto-parse-filter-values/auto-parse-filter-values
    #'validate-temporal-bucketing/validate-temporal-bucketing
    #'optimize-temporal-filters/optimize-temporal-filters
+   #'qp.constraints/maybe-add-default-userland-constraints
    #'limit/add-default-limit
    #'qp.middleware.enterprise/apply-download-limit
    #'check-features/check-features])
@@ -382,7 +383,7 @@
     (f (f query rff context)) -> (f query rff context)"
   (concat
    default-middleware
-   [#'qp.constraints/add-default-userland-constraints
+   [#'qp.constraints/mark-needs-default-userland-constraints
     #'process-userland-query/process-userland-query
     #'catch-exceptions/catch-exceptions]))
 

src/metabase/query_processor/middleware/constraints.clj
@@ -59,16 +59,34 @@
 (defn- merge-default-constraints [constraints]
   (merge (default-query-constraints) constraints))
 
-(defn- add-default-userland-constraints*
+(defn add-constraints
   "Add default values of `:max-results` and `:max-results-bare-rows` to `:constraints` map `m`."
+  [query]
+  (update query :constraints (comp ensure-valid-constraints merge-default-constraints)))
+
+(defn- should-add-userland-constraints? [query]
+  (get-in query [:middleware ::add-userland-constraints?]))
+
+(defn maybe-add-default-userland-constraints
+  "If the query is marked as requiring userland constraints, actually calculate the constraints and add them to the
+  query."
+  [query]
+  (cond-> query
+    (should-add-userland-constraints? query) add-constraints))
+
+(defn- mark-needs-default-userland-constraints*
+  "Marks the query as requiring userland constraints. Note that we can't actually calculate the constraints yet, because
+  of this middleware's position in the middleware stack (we don't yet have access to db-local settings). So this is a
+  bit awkward: we mark the query as needing userland constraints, then later the
+  `maybe-add-default-userland-constraints` middleware actually calculates the relevant constraints"
   [{{:keys [add-default-userland-constraints?]} :middleware, :as query}]
   (cond-> query
-    add-default-userland-constraints? (update :constraints (comp ensure-valid-constraints merge-default-constraints))))
+    add-default-userland-constraints? (update :middleware assoc ::add-userland-constraints? true)))
 
-(defn add-default-userland-constraints
+(defn mark-needs-default-userland-constraints
   "Middleware that optionally adds default `max-results` and `max-results-bare-rows` constraints to queries, meant for
   use with [[metabase.query-processor/process-query-and-save-with-max-results-constraints!]], which ultimately powers
   most QP API endpoints."
   [qp]
   (fn [query rff context]
-    (qp (add-default-userland-constraints* query) rff context)))
+    (qp (mark-needs-default-userland-constraints* query) rff context)))

src/metabase/query_processor/middleware/limit.clj
@@ -39,7 +39,6 @@
     (add-limit max-rows query)
     query))
 
-
 ;;;; Post-processing
 
 (defn- limit-xform [max-rows rf]

modules/drivers/sqlserver/test/metabase/driver/sqlserver_test.clj
@@ -16,7 +16,6 @@
    [metabase.models :refer [Database]]
    [metabase.query-processor :as qp]
    [metabase.query-processor.interface :as qp.i]
-   [metabase.query-processor.middleware.constraints :as qp.constraints]
    [metabase.query-processor.timezone :as qp.timezone]
    [metabase.test :as mt]
    [toucan2.tools.with-temp :as t2.with-temp]))
@@ -401,9 +400,6 @@
                                        "\n"
                                        "SELECT COUNT(1) FROM @TEMP\n")}
                           mt/native-query
-                          ;; add default query constraints to ensure the default limit of 2000 is overridden by the
-                          ;; `:rowcount-override` connection property we defined in the details above
-                          (assoc :constraints (qp.constraints/default-query-constraints))
-                          qp/process-query
+                          qp/process-userland-query
                           mt/rows
                           ffirst))))))))

test/metabase/query_processor/middleware/constraints_test.clj
@@ -5,7 +5,9 @@
    [metabase.test :as mt]))
 
 (defn- add-default-userland-constraints [query]
-  (:pre (mt/test-qp-middleware qp.constraints/add-default-userland-constraints query)))
+  (qp.constraints/maybe-add-default-userland-constraints
+   (:pre
+    (mt/test-qp-middleware qp.constraints/mark-needs-default-userland-constraints query))))
 
 (deftest ^:parallel no-op-without-middleware-options-test
   (testing "don't do anything to queries without [:middleware :add-default-userland-constraints?] set"
@@ -14,7 +16,8 @@
 
 (deftest ^:parallel add-constraints-test
   (testing "if it is *truthy* add the constraints"
-    (is (= {:middleware  {:add-default-userland-constraints? true},
+    (is (= {:middleware  {:add-default-userland-constraints?         true
+                          ::qp.constraints/add-userland-constraints? true}
             :constraints {:max-results           @#'qp.constraints/default-aggregated-query-row-limit
                           :max-results-bare-rows @#'qp.constraints/default-unaggregated-query-row-limit}}
            (add-default-userland-constraints
@@ -28,7 +31,8 @@
 
 (deftest ^:parallel dont-overwrite-existing-constraints-test
   (testing "if it already has constraints, don't overwrite those!"
-    (is (= {:middleware  {:add-default-userland-constraints? true}
+    (is (= {:middleware  {:add-default-userland-constraints? true
+                          ::qp.constraints/add-userland-constraints? true}
             :constraints {:max-results           @#'qp.constraints/default-aggregated-query-row-limit
                           :max-results-bare-rows 1}}
            (add-default-userland-constraints
@@ -37,15 +41,18 @@
 
 (deftest ^:parallel max-results-bare-rows-should-be-less-than-max-results-test
   (testing "if you specify just `:max-results` it should make sure `:max-results-bare-rows` is <= `:max-results`"
-    (is (= {:middleware  {:add-default-userland-constraints? true}
+    (is (= {:middleware  {:add-default-userland-constraints? true
+                          ::qp.constraints/add-userland-constraints? true}
             :constraints {:max-results           5
                           :max-results-bare-rows 5}}
            (add-default-userland-constraints
             {:constraints {:max-results 5}
-             :middleware  {:add-default-userland-constraints? true}}))))
+             :middleware  {:add-default-userland-constraints? true
+                           ::qp.constraints/add-userland-constraints? true}}))))
 
   (testing "if you specify both it should still make sure `:max-results-bare-rows` is <= `:max-results`"
-    (is (= {:middleware  {:add-default-userland-constraints? true}
+    (is (= {:middleware  {:add-default-userland-constraints? true
+                          ::qp.constraints/add-userland-constraints? true}
             :constraints {:max-results           5
                           :max-results-bare-rows 5}}
            (add-default-userland-constraints